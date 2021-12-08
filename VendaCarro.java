ebcb56a8b2340625810f6ba3f68fd84a04f84feb
Cecília Rauen <88169337+ceciliarauen@users.noreply.github.com>
2021-12-08 16:30:35 -0300

    Update VendaCarro.java

--- VendaCarro.java
+++ VendaCarro.java
@@ -4,16 +4,16 @@ public class VendaCarro {
 
 	public static void main(String[] args) {
 		/*
-		 * FaÁa um programa que receba o valor de um carro mostre uma tabela com os
-		 * seguintes dados: preÁo final, quantidade de parcelas e valor da parcela.
+		 * Fa√ßa um programa que receba o valor de um carro mostre uma tabela com os
+		 * seguintes dados: pre√ßo final, quantidade de parcelas e valor da parcela.
 		 * 
 		 * 
-		 * O preÁo final para compra ‡ vista tem um desconto de 20%; A prazo a
+		 * O pre√ßo final para compra √† vista tem um desconto de 20%; A prazo a
 		 * quantidade de parcelas pode ser: 6, 12, 18, 24, 30, 36, 42, 48, 54 e 60.
 		 * 
-		 * Os percentuais de acrÈscimo s„o:
+		 * Os percentuais de acr√©scimo s√£o:
 		 * 
-		 * Parcelas AcrÈscimo 6 3% / 12 6% / 18 9% / 24 12% / 30 15% / 36 18% / 42 21% /
+		 * Parcelas Acr√©scimo 6 3% / 12 6% / 18 9% / 24 12% / 30 15% / 36 18% / 42 21% /
 		 * 48 24% / 54 27% / 60 30% 11cases
 		 */
 
@@ -22,16 +22,17 @@ public class VendaCarro {
 		
 		vlrCarro = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor do carro"));
 		nmrParcelas = Integer.parseInt(JOptionPane.showInputDialog("Informe em quantas vezes de deseja parcelar \n"
-				+ "1x - 20% de desconto" + "\n" + "6x - 3% de acrÈscimo" + "\n" + "12x - 6% de acrÈscimo" + "\n"
-				+ "18x - 9% de acrÈscimo" + "\n" + "24x - 12% de acrÈscimo" + "\n" + "30x - 15% de acrÈscimo" + "\n"
-				+ "36x - 18% de acrÈscimo" + "\n" + "42x - 21% de acrÈscimo" + "\n" + "48x - 24% de acrÈscimo" + "\n"
-				+ "54x - 27% de acrÈscimo" + "\n" + "60x - 30% de acrÈscimo" + "\n"));
+				+ "1x - 20% de desconto" + "\n" + "6x - 3% de acr√©scimo" + "\n" + "12x - 6% de acr√©scimo" + "\n"
+				+ "18x - 9% de acr√©scimo" + "\n" + "24x - 12% de acr√©scimo" + "\n" + "30x - 15% de acr√©scimo" + "\n"
+				+ "36x - 18% de acr√©scimo" + "\n" + "42x - 21% de acr√©scimo" + "\n" + "48x - 24% de acr√©scimo" + "\n"
+				+ "54x - 27% de acr√©scimo" + "\n" + "60x - 30% de acr√©scimo" + "\n"));
 
 		switch (nmrParcelas) {
 		case 1: {
 			percentual = 20.0f / 100;
 			vlrFinal = vlrCarro - (vlrCarro * percentual);
-			System.out.println("O valor final ficou R$" + vlrFinal + " ‡ vista");
+			System.out.println("O valor final ficou R$" + vlrFinal + " √† vista");
+			break;
 		}
 		case 6: {
 			percentual = 3.0f / 100;
@@ -43,8 +44,8 @@ public class VendaCarro {
 			percentual = 6.0f / 100;
 			vlrFinal = vlrCarro + (vlrCarro * percentual);
 			vlrParcela = vlrFinal/nmrParcelas;
-			 System.out.println("O valor final ficou R$" + vlrFinal + ", em " + nmrParcelas + "x de " + vlrParcela);
-			 break;
+			System.out.println("O valor final ficou R$" + vlrFinal + ", em " + nmrParcelas + "x de " + vlrParcela);
+			break;
 		}
 		case 18: {
 			percentual = 9.0f / 100;
@@ -103,7 +104,7 @@ public class VendaCarro {
 			break;
 		
 		}default:
-			JOptionPane.showMessageDialog(null, "OpÁ„o Inv·lida");
+			JOptionPane.showMessageDialog(null, "Op√ß√£o Inv√°lida");
 		}
 		}
 	}
